4676668ecd7b50b8ebb670efa2c90d53b4f0f442
Johnny Do <dojohnny847@gmail.com>
2023-08-21 14:49:38 -0700

    Completed the first set of problems problem 0. Learned about conditionals more and how to use operators

--- problems/0.js
+++ problems/0.js
@@ -9,7 +9,17 @@
  * @example 92 -> "A", 75 -> "B"
 */
 function problem(percentage) {
-    return null;
+    if (percentage <=100 && percentage >= 90) {
+        return 'A';
+    } else if (percentage <=89 && percentage >= 80) {
+        return 'B';
+    } else if (percentage <=79 && percentage >= 70) {
+        return 'C';
+    } else if (percentage <=69 && percentage >= 60) {
+        return 'D'
+    } else if (percentage <=59 && percentage >= 0) {
+        return 'F'
+    }
 }
 
 const tests= [
