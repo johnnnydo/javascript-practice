3e5f06649784279707eb0f64902bc56f1af25528
Johnny Do <dojohnny847@gmail.com>
2023-08-24 01:33:57 -0700

    Finished prob 14. Confused but ended up creating a function which gets count of an item in the array given an array and which item you want. Then I created a varaible which rounds up the n/2 since it has to be at least and I checked if it is equal in a for loop

--- problems/14.js
+++ problems/14.js
@@ -7,7 +7,16 @@
  * @example [3,2,3] -> 3
 */
 function problem(numbers) {
-    return null;
+    const len_div = Math.ceil(numbers.length/2)
+    const item_counter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
+    for (const ele of numbers){
+        if (item_counter(numbers, ele) == len_div) {
+            return ele
+            break;
+        }
+    }
 }
 
 const tests = [
